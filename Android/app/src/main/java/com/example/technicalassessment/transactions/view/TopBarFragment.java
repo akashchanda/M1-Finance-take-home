5dd48cb7ad128f81ee95dcb7dcb26c28b1d64529
akashchanda <akash.chanda@gmail.com>
2021-03-14 15:51:47 -0400

    Add styling to title and sort options dropdown

--- Android/app/src/main/java/com/example/technicalassessment/transactions/view/TopBarFragment.java
+++ Android/app/src/main/java/com/example/technicalassessment/transactions/view/TopBarFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,14 +20,44 @@ public class TopBarFragment extends Fragment {
 
     private static final String TAG = "TopBar";
 
+    private Spinner mSortingOptionsDropdown;
+
+    enum SORT_OPTION {
+        NONE,
+        DATE_ASCENDING,
+        DATE_DESCENDING,
+        AMOUNT_ASCENDING,
+        AMOUNT_DESCENDING;
+
+        static SORT_OPTION getDefault() {
+            return NONE;
+        }
+    }
+
+    interface TopBarFragmentInterface {
+        void onSortOptionSelected(SORT_OPTION sortOption);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_top_bar, container, false);
         view.setTag(TAG);
 
-        //TODO get controls view
+        initSortingOptionsDropdown(view);
 
         return view;
+    }
+
+    private void initSortingOptionsDropdown(View view) {
+        mSortingOptionsDropdown = view.findViewById(R.id.sorting_options);
+        String[] sortingOptions = {
+                getString(R.string.sort_date_ascending),
+                getString(R.string.sort_date_descending),
+                getString(R.string.sort_amount_ascending),
+                getString(R.string.sort_amount_descending)
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, sortingOptions);
+        mSortingOptionsDropdown.setAdapter(adapter);
     }
 }
